f9442d170f7ebff5a16f43a6c44ead76db6a66a1
Mitali Bhalla <mbhalla@redhat.com>
2022-08-08 09:20:12 +0530

    Update itertools_1.py

--- itertools_1.py
+++ itertools_1.py
@@ -10,3 +10,12 @@ x=123%10
 re=123//10
 re=re*10+x
 print(re)
+
+
+# repeat() 
+     
+# importing "itertools" for iterator operations 
+import itertools 
+     
+# using repeat() to repeatedly print number 
+print (list(itertools.repeat(25, 4)))
